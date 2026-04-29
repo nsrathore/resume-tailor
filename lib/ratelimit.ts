89bcd351aedd87fc2a6d66c309f8a1d8916446c8
Nikhilendra Rathore <nikhilendra7@gmail.com>
2026-04-29 00:35:58 -0500

    Make rate limit an environment variable as well

--- lib/ratelimit.ts
+++ lib/ratelimit.ts
@@ -1,7 +1,9 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
+const limit = parseInt(process.env.RATE_LIMIT_PER_DAY ?? "5");
+
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(50, "1 d"), // 5 requests per IP per day
+  limiter: Ratelimit.slidingWindow(limit, "1 d"),
 });
